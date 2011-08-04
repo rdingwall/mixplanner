2f0a4e55ad5cca5b8a3eacbfa9ffe8c7a9f486d5
Richard Dingwall <rdingwall@gmail.com>
2011-08-04 14:14:30 +0100

    Moved TestHelper to separate file

--- src/Julana.Specs/DomainModel/SetSpecifications.cs
+++ src/Julana.Specs/DomainModel/SetSpecifications.cs
@@ -70,22 +70,4 @@ namespace Julana.Specs.DomainModel
             static Track trackToPlay;
         }
     }
-
-    public static class TestHelper
-    {
-        public static IEnumerable<Track> GetDummyTracks()
-        {
-            yield return new Track("A - A", Key.RandomKey());
-            yield return new Track("B - B", Key.RandomKey());
-            yield return new Track("C - C", Key.RandomKey());
-            yield return new Track("D - D", Key.RandomKey());
-            yield return new Track("E - E", Key.RandomKey());
-            yield return new Track("F - F", Key.RandomKey());
-            yield return new Track("G - G", Key.RandomKey());
-            yield return new Track("H - H", Key.RandomKey());
-            yield return new Track("I - I", Key.RandomKey());
-            yield return new Track("J - J", Key.RandomKey());
-            yield return new Track("K - K", Key.RandomKey());
-        }
-    }
 }
---
+++ src/Julana.Specs/DomainModel/TestHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Julana.CommandLine.DomainModel;
+
+namespace Julana.Specs.DomainModel
+{
+    public static class TestHelper
+    {
+        public static IEnumerable<Track> GetDummyTracks()
+        {
+            yield return new Track("A - A", Key.RandomKey());
+            yield return new Track("B - B", Key.RandomKey());
+            yield return new Track("C - C", Key.RandomKey());
+            yield return new Track("D - D", Key.RandomKey());
+            yield return new Track("E - E", Key.RandomKey());
+            yield return new Track("F - F", Key.RandomKey());
+            yield return new Track("G - G", Key.RandomKey());
+            yield return new Track("H - H", Key.RandomKey());
+            yield return new Track("I - I", Key.RandomKey());
+            yield return new Track("J - J", Key.RandomKey());
+            yield return new Track("K - K", Key.RandomKey());
+        }
+    }
+}
