5221912804407d2ef4cfb9c292bcef024494fc8a
Richard Dingwall <rdingwall@gmail.com>
2012-12-09 15:58:46 +0000

    CommandBase now logs invalid command parameter types.

--- src/MixPlanner/Commands/CommandBase.cs
+++ src/MixPlanner/Commands/CommandBase.cs
@@ -1,22 +1,42 @@
 ﻿using System;
 using System.Windows.Input;
+using log4net;
 
 namespace MixPlanner.Commands
 {
     public abstract class CommandBase<T> : ICommand
     {
+        protected readonly ILog Log;
+
+        protected CommandBase()
+        {
+            Log = LogManager.GetLogger(GetType());
+        }
+
         public bool CanExecute(object parameter)
         {
             if (!(parameter is T))
+            {
+                LogWrongParameterMessage(parameter);
                 return false;
+            }
 
             return DoCanExecute((T)parameter);
+        }
+
+        void LogWrongParameterMessage(object parameter)
+        {
+            Log.ErrorFormat("Invalid command parameter received, excepted {0} but got '{1}'",
+                            typeof(T), parameter);
         }
 
         public void Execute(object parameter)
         {
             if (!(parameter is T))
+            {
+                LogWrongParameterMessage(parameter);
                 return;
+            }
 
             DoExecute((T)parameter);
         }
