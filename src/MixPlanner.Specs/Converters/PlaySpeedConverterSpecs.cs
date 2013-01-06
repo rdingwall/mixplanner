4d4d2a40f56e7c45d707b9b07bce3c6885d97c9c
Richard Dingwall <rdingwall@gmail.com>
2013-01-06 17:05:43 +0000

    Renamed PlaySpeedConverter.

--- src/MixPlanner.Specs/Converters/PlaySpeedConverterSpecs.cs
+++ src/MixPlanner.Specs/Converters/PlaySpeedConverterSpecs.cs
@@ -3,42 +3,42 @@ using MixPlanner.Converters;
 
 namespace MixPlanner.Specs.Converters
 {
-    [Subject(typeof(PlaySpeedStringConverter))]
-    public class PlaySpeedStringConverterSpecs
+    [Subject(typeof(PlaySpeedConverter))]
+    public class PlaySpeedConverterSpecs
     {
          public class When_converting_a_positive_play_speed
          {
-             Establish context = () => converter = new PlaySpeedStringConverter();
+             Establish context = () => converter = new PlaySpeedConverter();
 
              Because of = () => result = converter.Convert(1.03, typeof (string), null, null);
 
              It should_display_it_as_a_percentage = () => result.ShouldEqual("+3%");
 
-             static PlaySpeedStringConverter converter;
+             static PlaySpeedConverter converter;
              static object result;
          }
 
          public class When_converting_a_negative_play_speed
          {
-             Establish context = () => converter = new PlaySpeedStringConverter();
+             Establish context = () => converter = new PlaySpeedConverter();
 
              Because of = () => result = converter.Convert(0.97, typeof(string), null, null);
 
              It should_display_it_as_a_percentage = () => result.ShouldEqual("-3%");
 
-             static PlaySpeedStringConverter converter;
+             static PlaySpeedConverter converter;
              static object result;
          }
 
          public class When_converting_the_default_play_speed
          {
-             Establish context = () => converter = new PlaySpeedStringConverter();
+             Establish context = () => converter = new PlaySpeedConverter();
 
              Because of = () => result = converter.Convert(1, typeof(string), null, null);
 
              It should_display_it_as_a_percentage = () => result.ShouldEqual("0%");
 
-             static PlaySpeedStringConverter converter;
+             static PlaySpeedConverter converter;
              static object result;
          }
     }
--- src/MixPlanner/Converters/PlaySpeedConverter.cs
+++ src/MixPlanner/Converters/PlaySpeedConverter.cs
@@ -5,7 +5,7 @@ using System.Windows.Data;
 namespace MixPlanner.Converters
 {
     [ValueConversion(typeof(double), typeof(string))]
-    public class PlaySpeedStringConverter : IValueConverter
+    public class PlaySpeedConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
