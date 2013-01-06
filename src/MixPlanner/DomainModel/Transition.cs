6e89f65075a9bc87256f78c0a68297da0462452b
Richard Dingwall <rdingwall@gmail.com>
2013-01-06 17:25:38 +0000

    Transition is now IComparable for sorting (using strategy description).

--- src/MixPlanner/DomainModel/Transition.cs
+++ src/MixPlanner/DomainModel/Transition.cs
@@ -2,12 +2,12 @@
 
 namespace MixPlanner.DomainModel
 {
-    public class Transition
+    public class Transition : IEquatable<Transition>, IComparable<Transition>, IComparable
     {
         public static Transition Intro(HarmonicKey toKey)
         {
             if (toKey == null) throw new ArgumentNullException("toKey");
-            return new Transition {ToKey = toKey};
+            return new Transition { ToKey = toKey };
         }
 
         public static Transition Outro(HarmonicKey fromKey)
@@ -16,12 +16,12 @@ namespace MixPlanner.DomainModel
             return new Transition { FromKey = fromKey };
         }
 
-        private Transition() {}
+        private Transition() { }
 
         public Transition(
-            HarmonicKey fromKey, 
-            HarmonicKey toKey, 
-            IMixingStrategy strategy, 
+            HarmonicKey fromKey,
+            HarmonicKey toKey,
+            IMixingStrategy strategy,
             double increaseRequired = 0)
         {
             if (fromKey == null) throw new ArgumentNullException("fromKey");
@@ -37,5 +37,51 @@ namespace MixPlanner.DomainModel
         public HarmonicKey ToKey { get; private set; }
         public IMixingStrategy Strategy { get; private set; }
         public double IncreaseRequired { get; private set; }
+
+        string Description
+        {
+            get { return Strategy != null ? Strategy.Description : ""; }
+        }
+
+        public bool Equals(Transition other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(ToKey, other.ToKey) && Equals(FromKey, other.FromKey) && Equals(Strategy, other.Strategy) && IncreaseRequired.Equals(other.IncreaseRequired);
+        }
+
+        public int CompareTo(Transition other)
+        {
+            if (other == null)
+                return 1;
+
+            // Sort by strategy descriptions if there are any
+            return Description.CompareTo(other.Description);
+        }
+
+        public int CompareTo(object obj)
+        {
+            return CompareTo(obj as Transition);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Transition)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (ToKey != null ? ToKey.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (FromKey != null ? FromKey.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Strategy != null ? Strategy.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ IncreaseRequired.GetHashCode();
+                return hashCode;
+            }
+        }
     }
 }
