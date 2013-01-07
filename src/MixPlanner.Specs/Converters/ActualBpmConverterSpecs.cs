d4ffa2144f0a0f0cbbab014019244af8c1c3516b
Richard Dingwall <rdingwall@gmail.com>
2013-01-07 23:02:10 +0000

    Fixed a minor bug I just spotted where adjusted BPMs were being displayed as 128.99999999999999999 on screen.

---
+++ src/MixPlanner.Specs/Converters/ActualBpmConverterSpecs.cs
@@ -0,0 +1,25 @@
+﻿using Machine.Specifications;
+using MixPlanner.Converters;
+
+namespace MixPlanner.Specs.Converters
+{
+    [Subject(typeof(ActualBpmConverter))]
+    public class ActualBpmConverterSpecs
+    {
+         public class When_converting_a_bpm_with_decimal_places
+         {
+             Because of = () => formatted = new ActualBpmConverter().Convert(128.934532, null, null, null);
+
+             It should_format_it_to_one_decimal_place = () => formatted.ShouldEqual("128.9");
+             static object formatted; 
+         }
+
+         public class When_converting_a_whole_number_bpm
+         {
+             Because of = () => formatted = new ActualBpmConverter().Convert(128, null, null, null);
+
+             It should_not_include_any_decimal_places = () => formatted.ShouldEqual("128");
+             static object formatted;
+         }
+    }
+}
---
+++ src/MixPlanner/Converters/ActualBpmConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MixPlanner.Converters
+{
+    [ValueConversion(typeof(double), typeof(string))]
+    public class ActualBpmConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            var bpm = System.Convert.ToDouble(value);
+
+            return string.Format("{0:0.#}", bpm);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
