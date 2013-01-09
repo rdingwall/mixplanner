fda04b0065d6a94c00e0e24bffefea78a3c434e3
Richard Dingwall <rdingwall@gmail.com>
2013-01-09 23:20:46 +0000

    Now you can only type 4 digits into the year textbox.

---
+++ src/MixPlanner/Controls/YearOnlyTextBox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace MixPlanner.Controls
+{
+    public class YearOnlyTextBox : TextBox
+    {
+        // Based on http://dedjo.blogspot.co.uk/2007/11/number-only-textbox.html
+        protected override void OnPreviewTextInput(System.Windows.Input.TextCompositionEventArgs e)
+        {
+            e.Handled = !e.Text.All(Char.IsDigit);
+            base.OnPreviewTextInput(e);
+        }
+    }
+}
