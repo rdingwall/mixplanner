e00a45868538628bcf035a34c96afafe714c5f13
Richard Dingwall <rdingwall@gmail.com>
2013-02-06 22:29:43 +0000

    Playing around with QuickGraph for intelligent insert, reorder etc.

---
+++ src/MixPlanner.Specs/GraphTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MixPlanner.DomainModel;
+using MixPlanner.DomainModel.MixingStrategies;
+using NUnit.Framework;
+using QuickGraph;
+using QuickGraph.Algorithms;
+
+namespace MixPlanner.Specs
+{
+    public class GraphTests
+    {
+        [Test]
+        public void Test()
+        {
+            var keys = new[]
+                           {
+                               HarmonicKey.Key2A,
+                               HarmonicKey.Key3A,
+                               HarmonicKey.Key5A,
+                               HarmonicKey.Key8A,
+                               HarmonicKey.Key8A,
+                               HarmonicKey.Key8A,
+                               HarmonicKey.Key10A
+                           }.Select(k => new PlaybackSpeed(k, 128));
+
+            var bpmRangeChecker = new BpmRangeChecker();
+            var strategies = new IMixingStrategy[]
+                {
+                    new SameKey(bpmRangeChecker), 
+                    new OneSemitone(bpmRangeChecker),
+                    new PerfectFifth(bpmRangeChecker),
+                    new TwoSemitoneEnergyBoost(bpmRangeChecker)
+                };
+
+            var edges =
+                from startKey in keys
+                from endKey in keys
+                where !endKey.Equals(startKey)
+                from strategy in strategies
+                where strategy.IsCompatible(endKey, startKey)
+                select new StrategyEdge(endKey, startKey, strategy);
+
+            var graph = edges.ToAdjacencyGraph<PlaybackSpeed, StrategyEdge>();
+
+            Console.WriteLine("{0} edges, {1} vertices.", 
+                graph.EdgeCount, graph.VertexCount);
+
+            var pathCount = EulerianTrailAlgorithm<PlaybackSpeed, StrategyEdge>.ComputeEulerianPathCount(graph);
+            Console.WriteLine("Found {0} eulerian paths.", pathCount);
+
+            var algo = new EulerianTrailAlgorithm<PlaybackSpeed, StrategyEdge>(graph);
+
+            algo.AddTemporaryEdges((source, target) => new StrategyEdge(source, target, null));
+            algo.Compute();
+            var trails = algo.Trails();
+            algo.RemoveTemporaryEdges();
+
+            Console.WriteLine("Found {0} eulerian trails.", trails.Count);
+
+            foreach (ICollection<StrategyEdge> trail in trails)
+            {
+                
+            }
+        }
+
+        class StrategyEdge : Edge<PlaybackSpeed>
+        {
+            public IMixingStrategy Strategy { get; private set; }
+
+            public StrategyEdge(
+                PlaybackSpeed source, 
+                PlaybackSpeed target, 
+                IMixingStrategy strategy) : base(source, target)
+            {
+                Strategy = strategy;
+            }
+        }
+    }
+}
