0a32084ae2975afd6c52147a1ac5404b30f365a8
Richard Dingwall <rdingwall@gmail.com>
2013-03-23 12:34:59 +0000

    Adding named positional params

--- src/MixPlanner/DomainModel/UnknownKey.cs
+++ src/MixPlanner/DomainModel/UnknownKey.cs
@@ -2,7 +2,7 @@
 {
     public class UnknownKey : HarmonicKey
     {
-        public UnknownKey() : base(0, Scale.Unknown, -1) {}
+        public UnknownKey() : base(hexValue: 0, scale: Scale.Unknown, pitch: -1) {}
 
         public override string ToString()
         {
