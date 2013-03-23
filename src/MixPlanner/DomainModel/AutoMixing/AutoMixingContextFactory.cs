bb6b34c7a40ff85c3c0b727787b76c796713ab85
Richard Dingwall <rdingwall@gmail.com>
2013-03-23 18:29:32 +0000

    Code comments

--- src/MixPlanner/DomainModel/AutoMixing/AutoMixingContextFactory.cs
+++ src/MixPlanner/DomainModel/AutoMixing/AutoMixingContextFactory.cs
@@ -17,16 +17,20 @@ namespace MixPlanner.DomainModel.AutoMixing
             if (selectedTracks == null) throw new ArgumentNullException("selectedTracks");
 
             if (selectedTracks.Count() == mix.Count)
+            {
+                // Entire mix is selected, everything can be moved (no
+                // preceeeding/following keys that need to be observed)
                 return new AutoMixingContext(selectedTracks);
+            }
 
             IMixItem preceeding = mix.GetPreceedingItem(selectedTracks.First());
             IMixItem following = mix.GetFollowingItem(selectedTracks.Last());
 
+            // Only uphold preceeding/following keys if they are known.
             IgnoreIfUnknownKeyOrBpm(ref preceeding);
             IgnoreIfUnknownKeyOrBpm(ref following);
 
             return new AutoMixingContext(selectedTracks, preceeding, following);
-
         }
 
         void IgnoreIfUnknownKeyOrBpm(ref IMixItem track)
