0b86b5fc93f4c8b4155912d49be7cee0608de238
Richard Dingwall <rdingwall@gmail.com>
2013-05-06 20:08:57 +0100

    Added message explaining that when you remove tracks from the library, the underlying files won't be touched.

--- src/MixPlanner/Commands/RemoveTracksFromLibraryCommand.cs
+++ src/MixPlanner/Commands/RemoveTracksFromLibraryCommand.cs
@@ -35,7 +35,8 @@ namespace MixPlanner.Commands
         protected override void Execute(IEnumerable<Track> parameter)
         {
             var trackOrTracks = pluralizer.Pluralize(parameter, "track");
-            var content = String.Format("Are you sure you want to remove the selected {0} from the library?", trackOrTracks);
+            var content = String.Format("Are you sure you want to remove the selected {0} from the library?{1}{1}(The underlying files will not be deleted.)",
+                trackOrTracks, Environment.NewLine);
 
             var message =
                 new DialogMessage(this, content, m => OnRemoveConfirmed(m, parameter))
