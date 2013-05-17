a39912ac398cd8c4176aabfe33b688cdfe4fbf7a
Richard Dingwall <rdingwall@gmail.com>
2013-05-18 00:32:32 +0100

    Removed unneeded json attributes.

--- src/MixPlanner/Storage/JsonTrack.cs
+++ src/MixPlanner/Storage/JsonTrack.cs
@@ -5,23 +5,17 @@ namespace MixPlanner.Storage
 {
     public class JsonTrack
     {
-        [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
 
-        [JsonProperty(PropertyName = "artist")]
         public string Artist { get; set; }
 
-        [JsonProperty(PropertyName = "title")]
         public string Title { get; set; }
 
-        [JsonProperty(PropertyName = "key")]
         [JsonConverter(typeof(HarmonicKeyJsonConverter))]
         public HarmonicKey Key { get; set; }
 
-        [JsonProperty(PropertyName = "bpm")]
         public double Bpm { get; set; }
 
-        [JsonProperty(PropertyName = "filename")]
         public string Filename { get; set; }
     }
 }
