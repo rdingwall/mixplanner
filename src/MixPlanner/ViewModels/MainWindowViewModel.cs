d7ffbdc9bdd3d939cfe8a10390255cdd2109fd55
Richard Dingwall <rdingwall@gmail.com>
2013-09-17 21:13:16 +0100

    CTRL+O now brings up open dialog.

--- src/MixPlanner/ViewModels/MainWindowViewModel.cs
+++ src/MixPlanner/ViewModels/MainWindowViewModel.cs
@@ -11,6 +11,7 @@ namespace MixPlanner.ViewModels
         public MixSurroundingAreaViewModel MixSurroundingArea { get; private set; }
         public TrackLibraryViewModel TrackLibrary { get; private set; }
         public FocusSearchBoxCommand FocusSearchBoxCommand { get; private set; }
+        public OpenMixCommand OpenCommand { get; private set; }
         public AudioPlayerViewModel AudioPlayer { get; private set; }
 
         public MainWindowViewModel(
@@ -18,17 +19,20 @@ namespace MixPlanner.ViewModels
             MixSurroundingAreaViewModel mixSurroundingAreaViewModel,
             TrackLibraryViewModel trackLibraryViewModel,
             FocusSearchBoxCommand focusSearchBoxCommand,
-            AudioPlayerViewModel audioPlayer)
+            OpenMixCommand openCommand,
+            AudioPlayerViewModel audioPlayer) : base(messenger)
         {
             if (messenger == null) throw new ArgumentNullException("messenger");
             if (mixSurroundingAreaViewModel == null) throw new ArgumentNullException("mixSurroundingAreaViewModel");
             if (trackLibraryViewModel == null) throw new ArgumentNullException("trackLibraryViewModel");
             if (focusSearchBoxCommand == null) throw new ArgumentNullException("focusSearchBoxCommand");
+            if (openCommand == null) throw new ArgumentNullException("openCommand");
             if (audioPlayer == null) throw new ArgumentNullException("audioPlayer");
 
             MixSurroundingArea = mixSurroundingAreaViewModel;
             TrackLibrary = trackLibraryViewModel;
             FocusSearchBoxCommand = focusSearchBoxCommand;
+            OpenCommand = openCommand;
             AudioPlayer = audioPlayer;
 
             messenger.Register<DialogMessage>(this, OnDialogRequested);
